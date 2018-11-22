174e86ac84dffd193d33e169b6be5e7ef7c0cc1e
dpiskur <danielpiskur@gmail.com>
2018-11-22 13:51:42 +0100

    end

--- src/app/shared/preview.directive.ts
+++ src/app/shared/preview.directive.ts
@@ -6,12 +6,6 @@ import { Directive, ElementRef, Renderer2, HostListener, Input } from '@angular/
 })
 export class PreviewDirective {
 
-
-  // constructor(el: ElementRef, private sanitizer: DomSanitizer, private elementRef: ElementRef){
-  // this.elementRef.nativeElement.innerHTML =
-  // '<p><img src="https://angular.io/assets/images/logos/angular/logo-nav@2x.png"><p>';
-
-
   @Input()
   private urlLink: string;
 
@@ -29,10 +23,7 @@ export class PreviewDirective {
   @HostListener('mouseenter')
   mouseenter(eventImg: Event) {
     this.paragraph.innerHTML = '<img src="' + this.urlLink + '" height="200" width="200" >';
-
-    console.log(this.paragraph);
     this.renderer.appendChild(this.el.nativeElement, this.paragraph);
-    console.log(this.urlLink);
   }
 
   @HostListener('mouseleave')
